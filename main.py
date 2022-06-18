d230699cdaad5796afbb07e5f0592be88bd39a43
nsiceret <105164151+nsiceret@users.noreply.github.com>
2022-06-18 19:10:38 +0200

    boutons -site de site_url

--- main.py
+++ main.py
@@ -187,8 +187,8 @@ def define_env(env):
         Methods : Use an HTML input to upload a file from user. The user clicks on the button to fire a JS event
         that triggers the hidden input.
         """
-        path_img = convert_url_to_utf8(env.variables.page.abs_url).split('/')[1]
-        return f"""<button class="tooltip" onclick="document.getElementById('input_editor_{tc}').click()"><img src="/{path_img}/images/buttons/icons8-upload-64.png"><span class="tooltiptext">Téléverser</span></button>\
+        path_img = env.variables.config["site_url"]
+        return f"""<button class="tooltip" onclick="document.getElementById('input_editor_{tc}').click()"><img src="{path_img}/images/buttons/icons8-upload-64.png"><span class="tooltiptext">Téléverser</span></button>\
                 <input type="file" id="input_editor_{tc}" name="file" enctype="multipart/form-data" class="hide"/>"""
 
     def create_unittest_button(tc: str, nom_script: str, path : str, mode: str, MAX : int = 5) -> str:
