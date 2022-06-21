fcf4e8086769d590eb5c66a4459b19410eae53a0
nsiceret <105164151+nsiceret@users.noreply.github.com>
2022-06-21 18:21:58 +0200

    un teste d'exercice qui fonctionne presque

--- docs/TNSI/01ListesPilesFiles/index.md
+++ docs/TNSI/01ListesPilesFiles/index.md
@@ -1,5 +1,16 @@
 # Listes, piles et files
 
-Un exercice sur les piles :
+Un exercice sur les piles.
+
+Vous disposeez des fonctions `creer_pile_vide()` et `empiler(P,e)`. Vous pouvez vérifier votre programme avec `afficher_pile(P)`.
+
+Créez la pile :
+
+```python
+9
+7
+2
+4
+```
 
 {{IDE('TNSI/exoPiles')}}
--- site/scripts/TNSI/exoPiles.py
+++ site/scripts/TNSI/exoPiles.py
@@ -1,3 +1,38 @@
-import PilesL
-P = PilesL.creer_pile_vide()
-PilesL.empiler(P,9)
+#--- HDR ---#
+def creer_pile_vide():
+    """
+    Renvoie un objet de type list vide pour simuler une pile vide
+    """
+    return []
+
+def est_vide(pile):
+    """
+    Renvoie True si et seulement si pile est vide
+    """
+    return len(pile) == 0
+
+def empiler(pile,item):
+    """
+    Empile item sur pile
+    """
+    pile.append(item)
+      
+def depiler(pile):
+    """
+    Dépile la pile et renvoie l'élément enlevé
+    """
+    assert not est_vide(pile),"on ne peut pas dépiler une pile vide"
+    return pile.pop()    #fonction Python qui enlève et renvoie le dernier élément d'une 'list'
+def afficher_pile(p):
+    q  = creer_pile_vide()
+    print("---pile---")
+    while not est_vide(p):
+        e = depiler(p)
+        empiler(q,e)
+        print(e)
+    while not est_vide(q):
+        e = depiler(q)
+        empiler(p,e)
+#--- HDR ---#
+P = creer_pile_vide()
+empiler(P,12)
